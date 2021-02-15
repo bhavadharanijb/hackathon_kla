7b723673d9c08e1921018f44e44ed754580e146a
bhavadharanijb <bhavadharanijb@gmail.com>
2021-02-15 18:43:15 +0530

    final

--- program/input1.py
+++ program/input1.py
@@ -13,7 +13,7 @@ for k in range(1,data['die']['columns']+1):
 
     for i in range(len(image_data)):
         for j in range(1,len(image_data[0])-1):
-            next=image_data[i][j+1]
+            next=imagerr_data[i][j+1]
             current=image_data[i][j]
             prev = image_data[i][j-1]
             #if((current[0]==prev[0] and current[1]==prev[1] and current[2]==prev[2]) or (current[0]==next[0] and current[1]==next[1] and current[2]==next[2])):
--- program/input2.py
+++ program/input2.py
@@ -1,8 +1,8 @@
-import json
 from PIL import Image
-import numpy
-import collections as cc
+import json
 import csv
+import collections as cc
+import numpy as np
 
 def Exclusion(data):
     for exclusion in range(len(data['exclusion_zones'])):
@@ -15,43 +15,45 @@ def inExclusion(i,j):
         return True
     return False
 
-def detect_defect(data):
-    row = 0
-    while(row < data['die']['rows']*data['die']['columns']):
-        image_data = []
-        for i in range(row+1, data['die']['columns']+row+ 1):
-            im = Image.open('program/inputset2/wafer_image_' + str(i) + '.png')
-            image_data.append(numpy.array(im))
-        for care in range(len(data['care_areas'])):
-            for y in range(data['care_areas'][care]['top_left']['x'], data['care_areas'][care]['bottom_right']['x']):
-                for x in range(data['care_areas'][care]['bottom_right']['y'],data['care_areas'][care]['top_left']['y']):
-                    if(inExclusion(y,x) == False):
-                        d = cc.defaultdict(int)
-                        for i in range( data['die']['columns'] ):
-                            d[tuple(image_data[i][x][y])] += 1
-                        m = 0
-                        for z in d:
-                            if(d[z] > m):
-                                temp = z
-                                m = d[z]
-                        if m == 1:
-                            for i in range(data['die']['columns']):
-                                ans.append([i+row+1,y,len(image_data[i])-1-x])
-                        else:
-                            for i in range(data['die']['columns']):
-                                if(tuple(image_data[i][x][y]) != temp):
-                                    ans.append([i+row+1,y,len(image_data[i])-1-x])
-        row += data['die']['columns']
-    f.close()
-    return True
-
-ans = []
-f = open('program/inputset2/input.json' )
+f = open('inputset2/input.json' )
 data = json.load(f)
+
 d_ex = cc.defaultdict(int)
 Exclusion(data)
-detect_defect(data)
+l=[]
+care_bottom=[]
+care_top=[]
+exclusion_bottom=[]
+exclusion_top=[]
+count=0
+for ca in data['care_areas']:
+    care_top.append(ca['top_left'])
+    care_bottom.append(ca['bottom_right'])
+for ca in data['care_areas']:
+    exclusion_top.append(ca['top_left'])
+    exclusion_bottom.append(ca['bottom_right'])
+print(data['die']['columns'])
+for k in range(1,data['die']['columns']*data['die']['rows']+1):
 
-with open("final.csv", 'w') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerows(ans)
+    image= Image.open('inputset2/wafer_image_10'+'.png')
+    image_data = np.asarray(image)
+
+
+    for a in range(len(care_bottom)):
+        for i in range(care_bottom[a]['y'] , care_top[a]['y']):
+            for j in range(care_top[a]['x']-1,care_bottom[a]['x']):
+                next=image_data[i][j+1]
+                current=image_data[i][j]
+                prev = image_data[i][j-1]
+                if (inExclusion(1,j) == False):
+            #if((current[0]==prev[0] and current[1]==prev[1] and current[2]==prev[2]) or (current[0]==next[0] and current[1]==next[1] and current[2]==next[2])):
+                     if((list(current)==list(prev)) or (list(current)==list(next))):
+                        pass
+                     else:
+                        l.append([k,j,len(image_data)-i-1])
+
+
+with open('output_2.csv', 'w+',newline="") as op:
+    csv_writer = csv.writer(op)
+    for i in l:
+        csv_writer.writerow(i)
