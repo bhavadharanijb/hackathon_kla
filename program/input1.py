71504d1ce99a5a66c1751c8c30abd6d704bbf2ae
bhavadharanijb <bhavadharanijb@gmail.com>
2021-02-15 12:07:34 +0530

    3

--- program/input1.py
+++ program/input1.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 import json
+import csv
 
 f = open('input.json' )
 data = json.load(f)
@@ -10,13 +11,18 @@ for k in range(1,data['die']['columns']+1):
     image_data = np.asarray(image)
 
     print(len(image_data[0]))
-
-    for i in range(len(image_data)):
-        for j in range(1,len(image_data[0])-1):
-            current=image_data[i][j]
-            prev=image_data[i][j-1]
-            next=image_data[i][j+1]
-            if (current[0]==prev[0] and current[1]==prev[1] and current[2]==prev[2] and current[0]==next[0] and current[1]==next[1] and current[2]==next[2]):
-                pass
-            else:
-                print(k,i,j)
+    l=[]
+    with open('output.csv','w') as op:
+        csv_writer=csv.writer(op)
+        for i in range(len(image_data)):
+            for j in range(1,len(image_data[0])-1):
+                current=image_data[i][j]
+                prev=image_data[i][j-1]
+                next=image_data[i][j+1]
+                if ((current[0]==prev[0] and current[1]==prev[1] and current[2]==prev[2] ) or (current[0]==next[0] and current[1]==next[1] and current[2]==next[2])):
+                    pass
+                else:
+                    l.append([k,i,j])
+        for i in l:
+            csv_writer.writerow((i))
+    #csv_writer.close()
