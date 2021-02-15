94fac4fc37454a580d8f187badb2c6c48f75e4c5
bhavadharanijb <bhavadharanijb@gmail.com>
2021-02-15 11:18:35 +0530

    first1

--- program/level1.py
+++ program/level1.py
@@ -1,9 +1,30 @@
 import json
-from PIL import Image,ImageGrab
+from PIL import Image
+import numpy
+import collections as cc
 
-f = open('input.json')
-
+f = open('input.json' )
 data = json.load(f)
-img1=Image.open(r"wafer_image_1.png")
-img1.show()
+
+for i in range(1,data['die']['columns']+1):
+    im = Image.open('wafer_image_'+str(i)+'.png')
+    px = im.load()
+    width,height = im.size
+    numpydata = numpy.array(im)
+    ans = []
+    for x in range(len(numpydata)):
+        d = cc.defaultdict(int)
+        for y in range(len(numpydata[i])):
+            d[numpydata[x][y][0]] += 1
+        m = 0
+        for z in d:
+            if(d[z] > m):
+                temp = x
+                m = d[z]
+        for y in range(len(numpydata[i])):
+            if(numpydata[i][y][0] != temp):
+                ans.append([i,x,y])
+    print(ans)
+
+
 f.close()
